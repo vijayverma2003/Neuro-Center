f84d223110e0ce304c2ac83c8f5e13d7c572d371
Vijay <vijayvermakvp@gmail.com>
2022-09-24 14:49:39 +0530

    Adding ayurveda description page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./styles/styles.css";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import AyurvedaDescription from "./components/AyurvedaDescription";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <AyurvedaDescription />
     </>
   );
 }
