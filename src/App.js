8fce488a1916b8dc94a4ced8b3eaef5582e49013
Vijay <vijayvermakvp@gmail.com>
2022-09-24 13:39:32 +0530

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./styles/normalize.css";
 import "./styles/styles.css";
 
+import Navbar from "./components/Navbar";
+
 function App() {
-  return (
-    <div>
-      <h1>Dukh bhanjan ayurvedic neuro center</h1>
-      <h2>Dukh bhanjan ayurvedic neuro center</h2>
-      <h3>Dukh bhanjan ayurvedic neuro center</h3>
-      <h4>Dukh bhanjan ayurvedic neuro center</h4>
-      <p>Dukh bhanjan ayurvedic neuro center</p>
-      <button className="btn btn--secondary">Contact Us</button>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../images/logo.jpeg";
+
+function Navbar(props) {
+  return (
+    <nav>
+      <img className="nav--logo" src={logo} alt="" />
+      <ul className="nav--items">
+        <li>
+          <a href="#">About</a>
+          <a href="#">Reviews</a>
+          <a className="btn" href="#">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
