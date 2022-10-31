9e4a7b5b8f72f5adeee5d12b9b049cb35e7584b8
Vijay <vijayvermakvp@gmail.com>
2022-10-31 06:10:38 +0530

    Updating MeetTheDoctor component

--- src/components/MeetTheDoctor.jsx
+++ src/components/MeetTheDoctor.jsx
@@ -5,75 +5,55 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function MeetTheDoctor(props) {
-  useEffect(() => {
-    gsap.fromTo(
-      ["header"],
-      { y: 0, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 1.2,
-        stagger: 0.05,
-        scrollTrigger: {
-          trigger: ".doctor--page",
-          start: "-60%",
-        },
-      }
-    );
+  // useEffect(() => {
+  //   gsap.set(".doctor--image", { transformOrigin: "center" });
 
-    gsap.set(".doctor--image", { transformOrigin: "center" });
+  //   gsap.fromTo(
+  //     ".doctor--image",
+  //     { scale: 0.8 },
+  //     {
+  //       scale: 1,
+  //       duration: 1.4,
+  //       ease: "Power3.easeOut",
+  //       scrollTrigger: {
+  //         trigger: ".doctor--page",
+  //         start: "-60%",
+  //       },
+  //     }
+  //   );
 
-    gsap.fromTo(
-      ".doctor--image",
-      { scale: 0.8 },
-      {
-        scale: 1,
-        duration: 1.4,
-        ease: "Power3.easeOut",
-        scrollTrigger: {
-          trigger: ".doctor--page",
-          start: "-60%",
-        },
-      }
-    );
-
-    gsap.fromTo(
-      ".doctor--description",
-      { x: 400, opacity: 0 },
-      {
-        x: 0,
-        opacity: 1,
-        duration: 1.4,
-        ease: "Power3.easeOut",
-        scrollTrigger: {
-          trigger: ".doctor--page",
-          start: "-60%",
-        },
-      }
-    );
-  }, []);
+  //   gsap.fromTo(
+  //     ".doctor--description",
+  //     { x: 400, opacity: 0 },
+  //     {
+  //       x: 0,
+  //       opacity: 1,
+  //       duration: 1.4,
+  //       ease: "Power3.easeOut",
+  //       scrollTrigger: {
+  //         trigger: ".doctor--page",
+  //         start: "-60%",
+  //       },
+  //     }
+  //   );
+  // }, []);
 
   return (
-    <section className="block container doctor--page">
-      <header>
-        <h1>Meet the Doctor</h1>
+    <section className="container doctor--page">
+      <header className="doctor--header">
+        <h1 className="doctor--heading">Meet Ved Janak Raj</h1>
+        <p>B.A.M.S, M.D</p>
+        <p></p>
       </header>
-      <div className="doctor--page--content">
+      <div className="doctor--content">
         <img className="doctor--image" src="" alt="" />
-        <div className="doctor--description">
-          <h3 className="doctor--name">Dr. Janak Raj</h3>
-          <p className="doctor--qualification">
-            <i>BAMS, MD</i>
-          </p>
-          <p className="doctor--views">
-            "Hospitals have been making healthcare accessible to one and all and
-            Dukh Bhanjan Ayurvedic Neuro Centre in Dasuya, Hoshiarpur is one
-            such reliable hospital that is committed to providing expert medical
-            care. Having established a firm presence as a trusted name in
-            Hospitals, it is renowned for offering specialised services and
-            treatments."
-          </p>
-        </div>
+        <p className="doctor--description">
+          Hospitals have been making healthcare accessible to one and all and
+          Dukh Bhanjan Ayurvedic Neuro Centre in Dasuya, Hoshiarpur is one such
+          reliable hospital that is committed to providing expert medical care.
+          Having established a firm presence as a trusted name in Hospitals, it
+          is renowned for offering specialised services and treatments.
+        </p>
       </div>
     </section>
   );
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,32 +3,18 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import reviews from "../services/reviews.json";
 
-console.log(reviews);
-
 gsap.registerPlugin(ScrollTrigger);
 
 function Reviews(props) {
   useEffect(() => {
-    gsap.fromTo(
-      [".reviews--main"],
-      { x: -400, opacity: 0 },
-      {
-        x: 0,
-        opacity: 1,
-        duration: 1.4,
-        ease: "Power3.easeOut",
-        scrollTrigger: { trigger: ".reviews--page", start: "-80%" },
-      }
-    );
-
     gsap.set(".review", { transformOrigin: "center" });
 
     gsap.fromTo(
       [".review"],
-      { scaleX: 0.5, scaleY: 0.6 },
+      { scale: 0.6, opacity: 0 },
       {
-        scaleX: window.innerWidth <= 768 ? 1.0 : 0.7,
-        scaleY: window.innerWidth <= 768 ? 1.0 : 0.8,
+        scale: window.innerWidth >= 768 ? 1.0 : 0.7,
+        opacity: 1,
         duration: 1.4,
         ease: "Power3.easeOut",
         scrollTrigger: { trigger: ".reviews--page", start: "-80%" },
@@ -41,7 +27,7 @@ function Reviews(props) {
       <h1 className="reviews--heading">What people say about us?</h1>
       <div className="reviews">
         {reviews["reviews"].map((review) => (
-          <div className="review">
+          <div key={review.image} className="review">
             <header className="review--header">
               <img
                 className="review--image"
@@ -55,7 +41,9 @@ function Reviews(props) {
             </header>
             <div>
               {review.problems.map((problem) => (
-                <span className="review--problem">{problem}</span>
+                <span key={problem} className="review--problem">
+                  {problem}
+                </span>
               ))}
             </div>
             <p className="review--content">{review.description}</p>
